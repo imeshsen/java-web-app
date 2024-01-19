f71fd2354013bd1bf29b46022b194b02ed59ebf1
Imesh Senanayake <115970061+imeshsen@users.noreply.github.com>
2024-01-19 14:46:46 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
 
 @SpringBootApplication
 @RestController
@@ -15,6 +16,20 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello Docker World!";
+	  String SystemName;
+	  try {
+ 
+            // get system name
+            
+             SystemName = InetAddress.getLocalHost().getHostName();
+ 
+            // SystemName stores the name of the system
+            System.out.println("System Name : "
+                               + SystemName);
+        }
+        catch (Exception E) {
+            System.err.println(E.getMessage());
+        }
+    return SystemName;
   }
 }
